9d9d1943825f6ccb1c0f90af490cfbd711fdb97f
Dale Corns <drcorns@hotmail.com>
2015-08-06 17:25:30 -0700

    get some testing working

--- test/controllerMain_test.js
+++ test/controllerMain_test.js
@@ -3,12 +3,27 @@
  * Created by dcorns on 8/5/15.
  */
 'use strict';
+require('../app/app');
+require('angular-mocks');
 
 describe('controllerMain', function(){
-  beforeEach(function(){
-    var controllerMain = require('../controllers/controllerMain');
-  });
-  it('should create message I am Angular!!', function(){
-    expect('I am Angular!!').to.toEqual('I am Angular!!');
+  //mocks
+
+  beforeEach(angular.mock.module('app'));
+  var $controller, scope;
+  beforeEach(inject(function(_$controller_,_$rootScope_){
+    $controller = _$controller_;
+    scope = _$rootScope_.$new();
+    var controllerMain = $controller('controllerMain', {$scope: scope});
+  }));
+
+  describe('$scope.message', function(){
+    it('should have message equal to I am Angular!!', function(){
+      expect(scope.message).toEqual('I am Angular!!');
+    });
+    it('should be able to change message value', function(){
+      scope.message = 'Angular';
+      expect(scope.message).toEqual('Angular');
+    })
   });
 });
